de201edd20a7316fdd016e2a5e3176dbdc6320f2
Jonathan Hinkle <hello@hynkle.com>
2018-02-11 13:45:07 -0500

    SQ main

--- main.cr
+++ main.cr
@@ -1,10 +1,18 @@
 require "toml"
 
-class Profile
-  getter name : String
-  getter image : String
+class Image
+  getter source
 
-  def initialize(name, image)
+  def initialize(source : String)
+    @source = source
+  end
+end
+
+class Profile
+  getter name
+  getter image
+
+  def initialize(name : String, image : Image)
     @name = name
     @image = image
   end
@@ -15,7 +23,7 @@ class Service
   getter image
   getter codes
 
-  def initialize(profile : Profile, image : String, codes = [] of String)
+  def initialize(profile : Profile, image : Image, codes = [] of String)
     @profile = profile
     @image = image
     @codes = codes
@@ -27,17 +35,18 @@ config = TOML.parse(File.read("card.toml"))
 profiles = {} of String => Profile
 config["profiles"].as(Hash).each do |(name, attrs)|
   attrs = attrs.as(Hash)
-  image = attrs["image"].as(String)
+  image = Image.new(attrs["image"].as(String))
   profiles[name] = Profile.new(name, image)
 end
 
 services = {} of String => Service
 config["services"].as(Hash).each do |(identifier, attrs)|
   attrs = attrs.as(Hash)
-  image = attrs["image"].as(String)
+  image = Image.new(attrs["image"].as(String))
   codes = attrs["codes"].as(Array).map {|code| code.as(String)}
   profile_name = attrs["profile"].as(String)
   profile = profiles.fetch(profile_name)
   services[identifier] = Service.new(profile, image, codes)
 end
+
 
