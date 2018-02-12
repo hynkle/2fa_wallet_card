28b990a883d5e8b2d452ce39b19e94dd6fa7bc13
Jonathan Hinkle <hello@hynkle.com>
2018-02-11 19:50:21 -0500

    section spacing!

--- main.cr
+++ main.cr
@@ -3,6 +3,9 @@ require "xml"
 require "base64"
 
 CARD_SIZE = {2.125, 3.375}
+MARGIN_VERTICAL = 0.25
+MARGIN_HORIZONTAL = 0.25
+SECTION_SEPARATOR_HEIGHT_AS_PROPORTION_OF_LINE_HEIGHT = 1
 
 class Image
   getter source
@@ -95,40 +98,53 @@ config["services"].as(Hash).each do |(identifier, attrs)|
   services[identifier] = Service.new(profile, image, codes)
 end
 
-number_of_lines = services.sum { |(_, service)| service.codes.size }
+
+
+card_width, card_height = CARD_SIZE
+content_height = card_height - MARGIN_VERTICAL * 2
+
+number_of_codes = services.sum { |(_, service)| service.codes.size }
+number_of_section_separators = services.size - 1
+units_of_vertical_space = number_of_codes + number_of_section_separators * SECTION_SEPARATOR_HEIGHT_AS_PROPORTION_OF_LINE_HEIGHT
+line_height = content_height.fdiv(units_of_vertical_space)
+section_separator_height = line_height * SECTION_SEPARATOR_HEIGHT_AS_PROPORTION_OF_LINE_HEIGHT
+image_height = image_width = line_height
+
+left_edge = MARGIN_HORIZONTAL
+v_cursor = MARGIN_VERTICAL
 
 svg = XML.build(indent: "  ") do |xml|
-  card_width, card_height = CARD_SIZE
   viewbox = [0, 0, card_width, card_height].join(" ")
-  line_height = card_height.fdiv(number_of_lines)
-  image_height = image_width = (line_height * 0.8)
-
   xml.dtd("svg", "-//W3C//DTD SVG 1.1//EN", "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd")
   xml.element("svg", xmlns: "http://www.w3.org/2000/svg", "xmlns:xlink": "http://www.w3.org/1999/xlink", viewBox: viewbox) do
     xml.element("rect", width: "100%", height: "100%", fill: "#EEEEEE")
 
-    line_number = 0
-    services.each do |(_, service)|
-      service.codes.each_with_index do |code, i|
-        is_first = i == 0
-
-        line_top = line_height * line_number
-
+    services.each.with_index do |(_, service), service_idx|
+      service.codes.each_with_index do |code, code_idx|
         xml.element("g", class: "row") do
-          if i == 0
-            xml.element("image", x: 0, y: line_top, width: image_width, height: image_height, "xlink:href": service.image.data_uri)
-            xml.element("image", x: line_height, y: line_top, width: image_width, height: image_height, "xlink:href": service.profile.image.data_uri)
+          if code_idx == 0
+            xml.element("image", x: left_edge, y: v_cursor, width: image_width, height: image_height, "xlink:href": service.image.data_uri)
+            xml.element("image", x: left_edge + image_width * 1.2, y: v_cursor, width: image_width, height: image_height, "xlink:href": service.profile.image.data_uri)
           end
 
-          xml.element("text", x: line_height * 2, y: line_top + (line_height * 0.5), "font-size": line_height * 0.5, "font-family": "DejaVu Sans Mono", "alignment-baseline": "middle") do
+          xml.element("text", x: left_edge + (image_width * 1.2 * 2), y: v_cursor + (line_height * 0.5), "font-size": line_height * 0.5, "font-family": "DejaVu Sans Mono", "alignment-baseline": "middle") do
             xml.text code
           end
         end
-
-        line_number += 1
+        v_cursor += line_height # printed out a line, so need to move down by a line
       end
+      v_cursor += section_separator_height
     end
   end
 end
+
+
+# puts "card_height: #{card_height}"
+# puts "content_height: #{content_height}"
+# puts "number_of_codes: #{number_of_codes}"
+# puts "number_of_section_separators: #{number_of_section_separators}"
+# puts "line_height: #{line_height}"
+# puts "section_separator_height: #{section_separator_height}"
+# puts "v_cursor: #{v_cursor}"
 
 puts svg
